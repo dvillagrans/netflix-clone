3c251936ac3184e9225f98b53492328e7ec74558
Diego Villagran <diegovillasal@gmail.com>
2024-09-23 11:29:36 -0600

    creacion del main

---
+++ front-end/src/components/icons.jsx
@@ -0,0 +1,9 @@
+import { PlayIcon, InfoIcon, BellIcon, SearchIcon, ChevronDownIcon } from 'lucide-react';
+
+export const Icons = {
+  PlayIcon,
+  InfoIcon,
+  BellIcon,
+  SearchIcon,
+  ChevronDownIcon,
+};
